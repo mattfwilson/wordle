d433b1b5682de311c9be6fbeb826690c97f73465
mattfwilson <mattfwilson@gmail.com>
2022-02-10 13:12:06 -0500

    Added PyDictionary library and created feature that defines the word if you lose the game

--- wordle2.py
+++ wordle2.py
@@ -1,37 +1,42 @@
 # check to see if two of the same character is in the guess
 # stacking the result summary (needs list of lists -Andres)
+# Add in conditional check for if the guess word is < or > 5 characters
 
+from PyDictionary import PyDictionary
+
+lookup = PyDictionary()
 wordle = 'plebs'
 userInput = ''
-guess = ''
+guess = 'guess'
 gameContinue = True
 guessCount = 1
 
 def checkGuess(guess, word, outputGuess, count):
+    global guessCount
     guess_index = 0
     word_index = 0
     correctCount = 0
 
-    while guess_index < len(guess):                 # create var with list character/index for guess
+    while guess_index < len(guess): # create var with list character/index for guess
         guessLetter = guess[guess_index]
-        if guessLetter in word:                     # check to see a guess character in the wordle
+        if guessLetter in word: # check to see a guess character in the wordle
             outputGuess[guess_index] = '(' + guess[guess_index] + ')'
         guess_index += 1
 
-    while word_index < len(word):                   # create vars with list characters/index for guess AND wordle
+    while word_index < len(word): # create vars with list characters/index for guess AND wordle
         wordLetterPos = word[word_index]
         guessLetterPos = guess[word_index]
-        if guessLetterPos == wordLetterPos:         # check if positions of guess characters and wordle characters matche
+        if guessLetterPos == wordLetterPos: # check if positions of guess characters and wordle characters matche
             outputGuess[word_index] = '[' + guess[word_index] + ']'
             correctCount += 1
         word_index += 1
-    print(f'Result: {outputGuess}')
-    count += 1
+    print(f'Guess {guessCount}: {outputGuess}')
+    guessCount += 1
     return correctCount != len(word)
 
-while gameContinue == True:                         # keeps game running while user still has guesses
+while gameContinue == True: # keeps game running while user still has guesses
     if guessCount <= 6:
-        guess = input('What is your first guess? ')
+        guess = input(f'What is your guess? ({guessCount}/6) ')
         word_char = []
         guess_char = []
         output_lst = []
@@ -45,5 +50,7 @@ while gameContinue == True:                         # keeps game running while u
 
         gameContinue = checkGuess(guess_char, word_char, output_lst, guessCount)
     elif guessCount > 6:
-        print('You are out of turns. Game over.')
+        print(f'Game over! You used all 6 guesses. The word was "{wordle}"!')
+        dictResult = lookup.meaning(f'{wordle}')
+        print(f'Defintion of {wordle}: \n{dictResult}')
         quit()
