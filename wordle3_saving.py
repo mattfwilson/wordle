311adce26e5c4a368b533c1ed2d529d6d328f097
mattfwilson <mattfwilson@gmail.com>
2022-02-16 17:29:08 -0500

    Saving/loading functionality working correctly!

--- wordle3_saving.py
+++ wordle3_saving.py
@@ -1,6 +1,9 @@
-# check to see if two of the same character is in the guess
-# stacking the result summary (needs list of lists -Andres)
-# Add in conditional check for if the guess word is < or > 5 characters
+"""
+To-dos
+check to see if two of the same character is in the guess
+stacking the result summary (needs list of lists -Andres)
+Add in conditional check for if the guess word is < or > 5 characters
+"""
 
 from PyDictionary import PyDictionary
 import random
@@ -13,16 +16,6 @@ guess = 'guess'
 gameContinue = True
 guessCount = 1
 record = {'wins': 0, 'losses': 0}
-
-record['wins'] += 2
-pickle.dump(record, open('wins.dat', 'wb'))
-print(record)
-
-with open("test_words.txt", "r") as possibleWords: # get/create list out of 5-letter-words.txt
-    file_lines = possibleWords.read()
-    wordList = file_lines.split("\n")
-
-wordle = random.choice(wordList)
 
 def checkGuess(guess, word, outputGuess, count):
     global guessCount
@@ -46,6 +39,16 @@ def checkGuess(guess, word, outputGuess, count):
     guessCount += 1
     return correctCount != len(word)
 
+
+record = pickle.load(open('user_record.dat', 'rb'))
+print(f'Wins: {record["wins"]} | Losses: {record["losses"]}')
+
+with open("test_words.txt", "r") as possibleWords: # get/create list out of 5-letter-words.txt
+        file_lines = possibleWords.read()
+        wordList = file_lines.split("\n")
+
+wordle = random.choice(wordList)
+
 while gameContinue == True: # keeps game running while user still has guesses
     if guessCount <= 6:
         while len(guess) < 5 or len(guess) > 5: # checks to make sure the guess is 5 characters long
@@ -66,14 +69,14 @@ while gameContinue == True: # keeps game running while user still has guesses
         gameContinue = checkGuess(guess_char, word_char, output_lst, guessCount)
 
         if gameContinue == False:
-            print(f'You won! The word was "{wordle}"!')
+            print(f'You won! You figured out the word was "{wordle}"!')
             record["wins"] += 1
-            pickle.dump(record, open('wins.dat', 'wb'))
-            print(record)
+            pickle.dump(record, open('user_record.dat', 'wb'))
+            print(f'Wins: {record["wins"]} | Losses: {record["losses"]}')
             quit()
     elif guessCount > 6:
-        print(f'Game over! You used all 6 guesses. The word was "{wordle}"!')
+        print(f'Game over! You used all 6 guesses. The correct word was "{wordle}"!')
         record["losses"] += 1
-        pickle.dump(record, open('wins.dat', 'wb'))
-        print(record)
+        pickle.dump(record, open('user_record.dat', 'wb'))
+        print(f'Wins: {record["wins"]} | Losses: {record["losses"]}')
         quit()
