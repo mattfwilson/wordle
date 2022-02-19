c1d4dafbe0cadd7b6473405c36993e1d721fc145
mattfwilson <mattfwilson@gmail.com>
2022-02-18 19:42:48 -0500

    Replaced pickle with json saving

--- wordle3_saving.py
+++ wordle3_saving.py
@@ -1,22 +1,19 @@
-"""
-To-dos
-check to see if two of the same character is in the guess
-stacking the result summary (needs list of lists -Andres)
-Add in conditional check for if the guess word is < or > 5 characters
-"""
+# To-dos
+# check to see if two of the same character is in the guess
+# stacking the result summary (needs list of lists -Andres)
+# Add in conditional check for if the guess word is < or > 5 characters
 
-from PyDictionary import PyDictionary
 import random
-import pickle
+import json
 
-lookup = PyDictionary()
 wordle = ''
 userInput = ''
 guess = 'guess'
 gameContinue = True
 guessCount = 1
-record = {'wins': 0, 'losses': 0}
+record = {"wins": 0, "losses": 0}
 
+# validates guesses and outputs results
 def checkGuess(guess, word, outputGuess, count):
     global guessCount
     guess_index = 0
@@ -39,17 +36,21 @@ def checkGuess(guess, word, outputGuess, count):
     guessCount += 1
     return correctCount != len(word)
 
-
-record = pickle.load(open('user_record.dat', 'rb'))
+# loads wins/losses from json
+save = open('records.json', 'r')
+record = save.read()
+save.close()
+print(record)
 print(f'Wins: {record["wins"]} | Losses: {record["losses"]}')
 
+# loads words list and generates random selection
 with open("test_words.txt", "r") as possibleWords: # get/create list out of 5-letter-words.txt
         file_lines = possibleWords.read()
         wordList = file_lines.split("\n")
+        wordle = random.choice(wordList)
 
-wordle = random.choice(wordList)
-
-while gameContinue == True: # keeps game running while user still has guesses
+# keeps game running while user still has guesses
+while gameContinue == True:
     if guessCount <= 6:
         while len(guess) < 5 or len(guess) > 5: # checks to make sure the guess is 5 characters long
             print(f'Your guess word has to be five characters.')
@@ -71,12 +72,16 @@ while gameContinue == True: # keeps game running while user still has guesses
         if gameContinue == False:
             print(f'You won! You figured out the word was "{wordle}"!')
             record["wins"] += 1
-            pickle.dump(record, open('user_record.dat', 'wb'))
+            save = open('records.json', 'w')
+            save.write(record)
+            save.close()
             print(f'Wins: {record["wins"]} | Losses: {record["losses"]}')
             quit()
     elif guessCount > 6:
         print(f'Game over! You used all 6 guesses. The correct word was "{wordle}"!')
         record["losses"] += 1
-        pickle.dump(record, open('user_record.dat', 'wb'))
+        save = open('records.json', 'w')
+        save.write(record)
+        save.close()
         print(f'Wins: {record["wins"]} | Losses: {record["losses"]}')
         quit()
