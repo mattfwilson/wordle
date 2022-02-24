358ddb6fc0236a0f195782710f4e580d6fcf2752
mattfwilson <mattfwilson@gmail.com>
2022-02-24 14:57:42 -0500

    Updated code notes for clarity

--- wordle4_json.py
+++ wordle4_json.py
@@ -1,7 +1,7 @@
 # To-dos
 # check to see if two of the same character is in the GUESS
 # stacking the result summary (needs list of lists -Andres)
-# add in conditional check for if the GUESS word is < or > 5 characters
+# conditional check for < or > 5 characters not working after first guess
 
 import random
 import json
@@ -19,15 +19,19 @@ def check_guess(guess, word, output_guess):
     word_index = 0
     correct_count = 0
 
-    while guess_index < len(guess): # create var with list character/index for guess
+    # create var with list character/index for guess
+    while guess_index < len(guess):
         guess_letter = guess[guess_index]
-        if guess_letter in word: # check to see a GUESS character in the wordle
+        # check to see a GUESS character in the wordle
+        if guess_letter in word:
             output_guess[guess_index] = '(' + guess[guess_index] + ')'
         guess_index += 1
-    while word_index < len(word): # create vars with list characters/index for guess AND wordle
+    # create vars with list characters/index for guess AND wordle
+    while word_index < len(word):
         word_letter_pos = word[word_index]
         guess_letter_pos = guess[word_index]
-        if guess_letter_pos == word_letter_pos: # check if positions of GUESS characters and wordle characters match
+        # check if positions of GUESS characters and wordle characters match
+        if guess_letter_pos == word_letter_pos:
             output_guess[word_index] = '[' + guess[word_index] + ']'
             correct_count += 1
         word_index += 1
@@ -49,7 +53,8 @@ with open("test_words.txt", "r") as possible_words: # get/create list out of 5-l
 # continue running game while user still has available guesses
 while CONTINUE == True:
     if GUESS_COUNT <= 6:
-        if len(GUESS) < 5 or len(GUESS) > 5: # checks to make sure the GUESS is 5 characters long
+        # checks to make sure the GUESS is 5 characters long
+        if len(GUESS) < 5 or len(GUESS) > 5:
             print(f'Your GUESS word has to be five characters.')
             GUESS = input(f'What is your GUESS? ({GUESS_COUNT}/6) ')
         elif len(GUESS) > 5:
@@ -83,5 +88,3 @@ while CONTINUE == True:
             json.dump(RECORD, save)
         print(f'Wins: {RECORD["wins"]} | Losses: {RECORD["losses"]}')
         quit()
-
-# {"wins": 10, "losses": 100}
